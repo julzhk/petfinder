31e9a5668e851e7b7e7632ca57d829e4ade0d043
julian <julian.harley@gmail.com>
2022-08-07 23:11:39 -0300

    Clean out 'hello world' placeholders

---
+++ src/components/PetFinder.vue
@@ -0,0 +1,16 @@
+<script setup>
+import {ref} from 'vue'
+
+defineProps({
+  msg: String
+})
+
+const count = ref(1)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <div class="bg-gray-200">
+    <button class="rounded-full bg-blue-300" @click="count++">count is {{ count }}</button>
+  </div>
+</template>
